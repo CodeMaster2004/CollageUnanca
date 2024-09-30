599607defe53178d52489083ba298466b1a46c77
CodeMaster2004 <falexanderhp@gmail.com>
2024-09-30 01:26:31 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+ 
+
+    $('.valentines-day').click(function(){
+        window.location.href = "index2.html"
+       
+    });
+  });
+$(document).ready(function(){
+
+
+$('.imagenes').click(function(){
+    window.location.href = "index4.html"
+    
+});
+});
+
